6582109bc61c836af73dde52443e3ae2033c6d2b
CyrilCele <nkululeko.cyril.cele@gmail.com>
2025-08-03 22:36:37 +0200

    Add login, logout, and register views
    - Create URLs for the new views
    - Create templates for the new views
    - Create the User model and migrate it
    - Add the custom model to the settings file

--- moodlinker/settings.py
+++ moodlinker/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "tracker",
 ]
+
+AUTH_USER_MODEL = 'tracker.User'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,61 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User
 
 
 def index(request):
     return render(request, "index.html", {})
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, "login.html", {"message": "Invalid username and/or password!"})
+    else:
+        return render(request, "login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("home"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "register.html", {"message": "Password must match!"})
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "register.html", {"message": "Username exists!"})
+        login(request, user)
+        return HttpResponseRedirect(reverse("home"))
+    else:
+        return render(request, "register.html")
+
+# @login_required(login_url="login")
